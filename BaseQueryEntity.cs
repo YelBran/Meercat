5e351fa3a1bf5e442bd510d07fc6e5d0bc425df7
Yel <3198544721@qq.com>
2016-04-20 11:18:56 +0800

    Added files via upload

---
+++ BaseQueryEntity.cs
@@ -0,0 +1,21 @@
+﻿using Hoomon.DataAccess.QueryAttribute;
+
+namespace Hoomon.DataAccess
+{
+    public abstract class BaseQueryEntity
+    {
+        [ConditionIgnore]
+        public int? PageIndex { get; set; }
+
+        [ConditionIgnore]
+        public int? PageSize { get; set; }
+
+        [ConditionIgnore]
+        [ExecuteIgnore]
+        public string SortField { get; set; }
+
+        [ConditionIgnore]
+        [ExecuteIgnore]
+        public string SortType { get; set; }
+    }
+}
