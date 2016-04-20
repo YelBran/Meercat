6d7e11f0566fd27a0de3785ba5b53ad4c695c902
Yel <3198544721@qq.com>
2016-04-20 14:16:59 +0800

    interceptors
    截断器

---
+++ app/interceptors.js
@@ -0,0 +1,21 @@
+﻿angular.module('interceptors', []).factory('authInterceptor', [
+  '$q', '$sessionStorage', 'apiService', '$location', 'city', function($q, $sessionStorage, apiService, $location, city) {
+    return {
+      request: function(config) {
+        if (config.url.indexOf('http://' + $location.$$host + ':8001/api/') > -1) {
+          config.headers['x-city'] = city.getCode();
+          if ($sessionStorage.user) {
+            config.headers['x-token'] = $sessionStorage.token;
+          }
+        }
+        return config || $q.when(config);
+      }
+    };
+  }
+]).config([
+  '$httpProvider', function($httpProvider) {
+    return $httpProvider.interceptors.push('authInterceptor');
+  }
+]);
+
+//# sourceMappingURL=interceptors.js.map
